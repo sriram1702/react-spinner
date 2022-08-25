a71fa74c27c4e55e94c8afea10fb2aaf0210941d
sriram1702 <ramsri1702@gmail.com>
2022-08-25 10:18:49 +0530

    react-spinner done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import PropagateLoader from 'react-spinners/PropagateLoader';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+  
+
+      <PropagateLoader color="white" size={50} />
     </div>
+    // </div> 
+
+    
   );
 }
 
